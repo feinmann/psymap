383ad6c3967284fccce01dba31d1ee1107e9ddf7
macbd <bidmaster@gmx.de>
2020-03-09 21:50:10 +0100

    Prepare for release 0.1.0

--- R/app_server.R
+++ R/app_server.R
@@ -4,8 +4,8 @@ app_server <- function(input, output,session) {
   output$mymap <- renderLeaflet({
     leaflet(data = join_festivals) %>% addTiles() %>%
       addMarkers(~long, ~lat, 
-                 popup = ~as.character(web), 
-                 label = ~as.character(paste(name, "<br>", place)),
+                 popup = ~as.character(paste(sep = "<br/>", name, place, web, start, end)), 
+                 label = ~as.character(paste(name)),
                  icon = makeIcon("www/om_icon.png", "www/am_icon.png@2x", 36, 36)) %>% 
       addProviderTiles(providers$Stamen.Toner)
   })
